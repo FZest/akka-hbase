a96e5242271695b7cce143a164252bd779d4c812
lunadancer@gmail.com <lichking123>
2016-02-18 15:21:57 +0800

    add temp

--- proxy/src/main/scala/io/github/junheng/akka/hbase/proxy/APP.scala
+++ proxy/src/main/scala/io/github/junheng/akka/hbase/proxy/APP.scala
@@ -4,7 +4,7 @@ import akka.actor.{ActorSystem, Props}
 import com.typesafe.config.ConfigFactory
 import io.github.junheng.akka.accessor.access.Accessor
 import io.github.junheng.akka.hbase.{HService, HTable}
-import io.github.junheng.akka.locator.{ServiceLocator, Service}
+import io.github.junheng.akka.locator.{Service, ServiceLocator}
 import io.github.junheng.akka.monitor.dispatcher.MonitoredForkJoinPool
 import io.github.junheng.akka.monitor.mailbox.SafeMailboxMonitor
 import org.apache.log4j.{Level, Logger}
@@ -21,7 +21,7 @@ object APP extends App {
 
   system.actorOf(Props(new SafeMailboxMonitor(config.getConfig("safe-mailbox-monitor"))), "safe-mailbox-monitor")
 
-  ServiceLocator.initialize("phb01,phb02,phb03")
+  ServiceLocator.initialize("phb01,phb02,phb03", "junheng")
 
   Accessor.start(config.getString("accessor.host"), config.getInt("accessor.port"))
 
