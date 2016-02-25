3800261162fe4f72180e910eab72c170c5e73979
lunadancer@gmail.com <lichking123>
2016-02-25 13:56:14 +0800

    switch to kyro

--- proxy/src/main/scala/io/github/junheng/akka/hbase/proxy/APP.scala
+++ proxy/src/main/scala/io/github/junheng/akka/hbase/proxy/APP.scala
@@ -4,7 +4,7 @@ import akka.actor.{ActorSystem, Props}
 import com.typesafe.config.ConfigFactory
 import io.github.junheng.akka.accessor.access.Accessor
 import io.github.junheng.akka.hbase.{HService, HTable}
-import io.github.junheng.akka.locator.{Service, ServiceLocator}
+import io.github.junheng.akka.locator.{LoadMonitor, Service, ServiceLocator}
 import io.github.junheng.akka.monitor.dispatcher.MonitoredForkJoinPool
 import io.github.junheng.akka.monitor.mailbox.SafeMailboxMonitor
 import org.apache.log4j.{Level, Logger}
@@ -13,13 +13,13 @@ object APP extends App {
 
   Logger.getRootLogger.setLevel(Level.OFF)
 
-  implicit val system = ActorSystem("proxy")
+  implicit val system = ActorSystem("hbase")
 
   implicit val config = ConfigFactory.load()
 
   MonitoredForkJoinPool.logger(system.log)
 
-  system.actorOf(Props(new SafeMailboxMonitor(config.getConfig("safe-mailbox-monitor"))), "safe-mailbox-monitor")
+  LoadMonitor.monitorActorRef = system.actorOf(Props(new SafeMailboxMonitor(config.getConfig("safe-mailbox-monitor"))), "safe-mailbox-monitor")
 
   ServiceLocator.initialize("phb01,phb02,phb03")
 
